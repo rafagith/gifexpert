09bcbd1ddcc09a11e0951bbca1435c2ccbc16a19
r_rosales_b@hotmail.com <43044433+rafagith@users.noreply.github.com>
2020-09-30 00:04:53 -0600

    docs added

---
+++ docs/precache-manifest.c0b63d6a2a6f9c5e30b710ca7374bbfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8106d9b15786262960d6d98f56927801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d5ded738ac6793b2cf",
+    "url": "/static/css/main.34ab623b.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d5ded738ac6793b2cf",
+    "url": "/static/js/main.80ea663b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
